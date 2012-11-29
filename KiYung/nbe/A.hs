6de16a9c21d427618de3755fda6760b03049e203
kyagrd@gmail.com <kyagrd@gmail.com@016002e3-b56b-4f38-5f20-9d2afd26562b>
2012-11-29 22:37:13 +0000

    some more scribble on nbe

--- KiYung/nbe/A.hs
+++ KiYung/nbe/A.hs
@@ -5,34 +5,37 @@
 
 data Nat = Z | S Nat
 
+infixr :>
 infixr :->
-infixr :*:
+-- infixr :*:
 
-data Ty :: * where
-  Iota :: Ty
-  (:->) :: Ty  -> Ty -> Ty
-  (:*:) :: Ty -> Ty -> Ty
-    deriving Show
+data T = I | T :> T
 
-data Tm = Var String | Lam String Tm | App Tm Tm
-        | Pair Tm Tm | Fst Tm | Snd Tm
-    deriving Show
+data Ty :: T -> * where
+  Iota :: Ty I
+  (:->) :: Ty a -> Ty b -> Ty (a:>b)
 
-data Val = LAM (Val -> Val) | PAIR (Val,Val) | SYN Tm
+data Tm :: T -> * where
+  Var :: String -> Tm a
+  Lam :: String -> Tm b -> Tm (a:>b)
+  App :: Tm (a:>b) -> Tm a -> Tm b
 
-type Ctx = [(String,Val)]
+data Val :: T -> * where
+  LAM :: (Val a -> Val b) -> Val (a:>b)
+  SYN :: Tm I -> Val I
 
 vars = map (:[]) cs ++ [c:show n | c<-cs, n<-[0..]] where cs="xyz"
 
-reflect :: Ty -> Tm -> Val
+reflect :: Ty t -> Tm t -> Val t
 reflect Iota        e = SYN e
 reflect (t1 :-> t2) e = LAM (\v -> reflect t2 (App e (reify t1 v vars)))
-reflect (t1 :*: t2) e = PAIR (reflect t1 (Fst e), reflect t2 (Snd e))
 
-reify :: Ty -> Val -> [String] -> Tm
+reify :: Ty t -> Val t -> [String] -> Tm t
 reify Iota        (SYN t) _      = t
 reify (t1 :-> t2) (LAM v) (x:xs) = Lam x (reify t2 (v (reflect t1 (Var x))) xs)
-reify (t1 :*: t2) (PAIR(v1,v2)) ss = Pair (reify t1 v1 ss) (reify t2 v2 ss)
+
+{-
+type Ctx = [(String,Val)]
 
 meaning :: Ctx -> Tm -> Val
 meaning ctx (Var x)      = case lookup x ctx of Just v -> v
@@ -41,11 +44,6 @@ meaning ctx (Lam x e)    = LAM (\v -> meaning ((x,v):ctx) e)
 meaning ctx (App e1 e2)  = case meaning ctx e1 of
                              LAM v1 -> v1 (meaning ctx e2)
                              _ -> undefined
-meaning ctx (Pair e1 e2) = PAIR (meaning ctx e1, meaning ctx e2)
-meaning ctx (Fst e)      = case meaning ctx e of PAIR p -> fst p
-                                                 _ -> undefined
-meaning ctx (Snd e)      = case meaning ctx e of PAIR p -> snd p
-                                                 _ -> undefined
 
 nbe :: Ty -> Tm -> Tm
 nbe t e = reify t (meaning [] e) vars
@@ -71,4 +69,4 @@ tmK' = nbe (Iota :-> (Iota :-> Iota) :-> Iota) k
 tmK'' = nbe ((Iota :-> Iota) :-> Iota :-> (Iota :-> Iota)) k
 
 tmS = nbe ((Iota :-> Iota :-> Iota) :-> (Iota :-> Iota) :-> Iota :-> Iota) s
-
+-}
